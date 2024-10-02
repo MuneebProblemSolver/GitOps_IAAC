e2711314b8bc7fdf510905105cf953d3d598efcb
MuneebProblemSolver <bcsm-f21-258@superior.edu.pk>
2024-10-02 11:43:17 +0500

    update env

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "cloudcore007"
+    bucket = "gitopsbucket01"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 
   required_version = ">= 1.6.3"
