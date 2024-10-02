0256c4ba1e909615df44a3595ac91758434512c4
MuneebProblemSolver <bcsm-f21-258@superior.edu.pk>
2024-10-02 17:45:45 +0500

    New

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -44,13 +44,13 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0756462d6720dfaa6" // Replace with the latest AMI ID for your region
+  default     = "ami-0c8eea98010057bd0" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t2.large"
 }
 
 variable "key_name" {
